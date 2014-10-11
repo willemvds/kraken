9a5f849999a0957235163e608da4ae4a9bf6a695
Willem van der Schyff <willemvds@gmail.com>
2014-10-11 17:21:54 +0200

    Clarify some things and add Complete to clean up

--- core.go
+++ core.go
@@ -7,12 +7,12 @@ import (
 	"code.google.com/p/go.crypto/ssh"
 )
 
-var ErrStop = errors.New("Stop Executing")
+var ErrNoMoreCommands = errors.New("No More Commands")
 
 type jobStatus int
 
 const (
-	JOB_COMPLETED jobStatus = iota
+	JOB_NO_MORE_COMMANDS jobStatus = iota
 	JOB_REMOTE_CONNECTION_CLOSED
 )
 
@@ -38,14 +38,14 @@ func (job *Job) Start() (<-chan jobStatus, error) {
 	if err := job.sshclient.Connect(); err != nil {
 		return nil, err
 	}
-	if err := job.StartCommandLoop(); err != nil {
+	if err := job.startCommandLoop(); err != nil {
 		return nil, err
 	}
 	job.statusChan = make(chan jobStatus)
 	return job.statusChan, nil
 }
 
-func (job *Job) StartCommandLoop() error {
+func (job *Job) startCommandLoop() error {
 	go func() {
 		buffer := make([]byte, 100)
 		for n, err := job.sshclient.Read(buffer); err == nil || n > 0; n, err = job.sshclient.Read(buffer) {
@@ -57,10 +57,14 @@ func (job *Job) StartCommandLoop() error {
 		job.statusChan <- JOB_REMOTE_CONNECTION_CLOSED
 	}()
 	go func() {
-		for command, err := job.commander.Command(); err != ErrStop; command, err = job.commander.Command() {
+		for command, err := job.commander.Command(); err != ErrNoMoreCommands; command, err = job.commander.Command() {
 			job.sshclient.Write(command)
 		}
-		job.statusChan <- JOB_COMPLETED
+		job.statusChan <- JOB_NO_MORE_COMMANDS
 	}()
 	return nil
+}
+
+func (job *Job) Complete() error {
+	return job.sshclient.Close()
 }
